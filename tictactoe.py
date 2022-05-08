44caacee54de5da128c2d23cbfb4287e82214de5
Shreyash Shinkar <shreyash.shinkar04@gmail.com>
2022-05-08 23:41:58 +0530

    Practice done

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 def sum(a, b, c):
+    # Overriding the inbuilt sum() function
     return a + b + c
 
 def printBoard(xState, zState):
@@ -41,6 +42,7 @@ def printBoard(xState, zState):
     print(f"{six} | {seven} | {eight} ") 
 
 def checkWin(xState, zState):
+    # Function with all the winning possibilities for a player
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for win in wins:
         if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
